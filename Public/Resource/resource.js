abbf6592da3175e224e316acb283cd0adea4c7fa
ShoKozuki <100390821+ShoKozuki@users.noreply.github.com>
2022-06-01 16:28:20 +1000

    I have fixed the mainpage structure and add kanban board to it

--- Public/Resource/resource.js
+++ Public/Resource/resource.js
@@ -1,6 +1,6 @@
 document.querySelector('.btn').addEventListener('click', function(){
     let title = document.querySelector('#title').value
-    let link = document.querySelector('link').value
+    let link = document.querySelector('#link').value
     let ul = document.querySelector('.ullist')
     let newlist = document.createElement('li');
     newlist.innerHTML = `<span class="span1">${title}</span><span class="span2">${link}</span>`
